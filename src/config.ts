ea9123959904d9a22fe8b6effff35fffde2ef3d2
ChaseRensberger <chasehrensberger@gmail.com>
2024-08-25 00:19:06 -0400

    some initial config

--- src/config.ts
+++ src/config.ts
@@ -1,11 +1,11 @@
 import type { Site, SocialObjects } from "./types";
 
 export const SITE: Site = {
-  website: "https://astro-paper.pages.dev/", // replace this with your deployed domain
-  author: "Sat Naing",
-  profile: "https://satnaing.dev/",
-  desc: "A minimal, responsive and SEO-friendly Astro blog theme.",
-  title: "AstroPaper",
+  website: "https://blog.chaserensberger.com/", // replace this with your deployed domain
+  author: "Chase Rensberger",
+  profile: "https://chaserensberger.com/",
+  desc: "A personal blog by Chase Rensberger",
+  title: "Chase Renberger's Blog",
   ogImage: "astropaper-og.jpg",
   lightAndDarkMode: true,
   postPerIndex: 4,
@@ -28,7 +28,7 @@ export const LOGO_IMAGE = {
 export const SOCIALS: SocialObjects = [
   {
     name: "Github",
-    href: "https://github.com/satnaing/astro-paper",
+    href: "https://github.com/chaserensberger",
     linkTitle: ` ${SITE.title} on Github`,
     active: true,
   },
@@ -36,17 +36,17 @@ export const SOCIALS: SocialObjects = [
     name: "Facebook",
     href: "https://github.com/satnaing/astro-paper",
     linkTitle: `${SITE.title} on Facebook`,
-    active: true,
+    active: false,
   },
   {
     name: "Instagram",
     href: "https://github.com/satnaing/astro-paper",
     linkTitle: `${SITE.title} on Instagram`,
-    active: true,
+    active: false,
   },
   {
     name: "LinkedIn",
-    href: "https://github.com/satnaing/astro-paper",
+    href: "https://linkedin.com/in/chaserensberger",
     linkTitle: `${SITE.title} on LinkedIn`,
     active: true,
   },
